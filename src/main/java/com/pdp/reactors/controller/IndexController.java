656d2ff930f5791d1f6bd56dcadc65baad463b5e
Parashar Devashish <devdewance@gmail.com>
2022-02-06 22:22:13 +0530

    Version controller added

---
+++ src/main/java/com/pdp/reactors/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.pdp.reactors.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("version")
+    public String getAppDeployedVersion(){
+        return  environment.getProperty("spring.application.version");
+    }
+}
